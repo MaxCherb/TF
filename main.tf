7943b2b07a6380554074df92d316f10d47d879ec
Maksim <mchernikov@evrone.team>
2024-10-11 16:18:41 +0300

    eto menya ubyet

--- main.tf
+++ main.tf
@@ -72,36 +72,36 @@ resource "yandex_vpc_address" "addr" {
 #   name = "storage-user"
 # }
 
-// Назначение роли сервисному аккаунту
-resource "yandex_resourcemanager_folder_iam_member" "sa-admin" {
-  folder_id = var.folder_id
-  role      = "storage.admin"
-  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
-}
+# // Назначение роли сервисному аккаунту
+# resource "yandex_resourcemanager_folder_iam_member" "sa-admin" {
+#   folder_id = var.folder_id
+#   role      = "storage.admin"
+#   member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+# }
 
-// Создание статического ключа доступа
-resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-  service_account_id = yandex_iam_service_account.sa.id
-  description        = "static access key for object storage"
-}
+# // Создание статического ключа доступа
+# resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+#   service_account_id = yandex_iam_service_account.sa.id
+#   description        = "static access key for object storage"
+# }
 
-// Создание бакета с использованием ключа
-resource "yandex_storage_bucket" "spaaaaace" {
-  access_key            = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key            = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  bucket                = "bucket-in-my-space"
-  acl                   = "private"
-  max_size              = 1024
-  default_storage_class = "COLD"
-  anonymous_access_flags {
-    read        = true
-    list        = false
-    config_read = false
-  }
+# // Создание бакета с использованием ключа
+# resource "yandex_storage_bucket" "spaaaaace" {
+#   access_key            = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+#   secret_key            = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+#   bucket                = "bucket-in-my-space"
+#   acl                   = "private"
+#   max_size              = 1024
+#   default_storage_class = "COLD"
+#   anonymous_access_flags {
+#     read        = true
+#     list        = false
+#     config_read = false
+#   }
   # tags = {
   #   <ключ_1> = "<значение_1>"
   #   <ключ_2> = "<значение_2>"
   #   ...
   #   <ключ_n> = "<значение_n>"
   # }
-}
+# }
